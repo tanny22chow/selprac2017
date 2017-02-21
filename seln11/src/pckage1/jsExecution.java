9dc64e7ebf84ccc622425dc911f8575ad312e0be
Tanmoy1991 <Tanmoy1991@DESKTOP-7HHGHG0>
2017-02-22 01:42:41 +0530

    2ndcommit

---
+++ seln11/src/pckage1/jsExecution.java
@@ -0,0 +1,21 @@
+package pckage1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class jsExecution {
+	static WebDriver d;
+	@Test
+	public static void open_browser()throws Exception{
+		System.setProperty("webdriver.chrome.driver","D:\\SELENIUM LEARNING\\DRIVERS\\chromedriver.exe");
+		d=new ChromeDriver();
+			d.get("http://www.seleniumhq.org/"); 
+			WebElement wb1=d.findElement(By.xpath(".//*[@id='mBody']/a[2]"));
+			JavascriptExecutor js=(JavascriptExecutor)d;
+			js.executeScript("window.scrollBy(0,200)");
+}
+}
